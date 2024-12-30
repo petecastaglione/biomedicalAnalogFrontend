f6f1d92bdeb322b77b3e5d70d85a92f95d26b1c3
Goia Iulian <goia.ma.iulian@student.utcluj.ro>
2024-12-30 17:44:35 +0200

    AO CP GBW 40MHz

--- p1_opamps/python/extract.py
+++ p1_opamps/python/extract.py
@@ -1,6 +1,6 @@
 from errorLogParser import *
 
-filepath= r"D:\Facultate\MASTER_AN_1\BLOCURI ANALOGICE AVANSATE\Backp_proj\AO_CP_NMOS\test_ao_cp_n_AC.log"
+filepath= r"D:\Facultate\MASTER_AN_1\BLOCURI ANALOGICE AVANSATE\Proiect\biomedicalAnalogFrontend\p1_opamps\AO_CP_NMOS\test_ao_cp_n_AC.log"
 mosfets = process_mosfet_data(filepath)
 
 for mos in mosfets:
